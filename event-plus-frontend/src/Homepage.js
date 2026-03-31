aa9ca4a87e37e857d1dd9009948f5d41730836de
Ahmad Arshad <ahmadarshad12345@gmail.com>
2026-03-31 11:14:01 +0100

    app must read env; app must move a archtecture style;

---
+++ event-plus-frontend/src/Homepage.js
@@ -0,0 +1,10 @@
+import "./index.css";
+
+export default function Homepage() {
+
+  return (
+    <div className="homepage-container">
+      <h1 className="homepage-title">Welcome to Event Plus</h1>
+      </div>
+  );
+}
