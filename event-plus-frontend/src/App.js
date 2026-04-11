c624786691484c339bec2fb7a30c9b96039cbf9e
gouravray <gourav.sankar93@gmail.com>
2026-04-11 11:10:21 +0100

    minor bug fixes required for project setup

--- event-plus-frontend/src/App.js
+++ event-plus-frontend/src/App.js
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
+import Header from "./components/header";
 import Footer from "./components/Footer";
 import Homepage from "./Homepage";
 import Notfound from "./Not_found";
