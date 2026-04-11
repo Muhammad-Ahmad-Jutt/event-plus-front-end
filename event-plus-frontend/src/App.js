45e8d992e506598b45eecfd39874e5c66a37a020
gouravray <gourav.sankar93@gmail.com>
2026-04-11 15:18:56 +0100

    Ap js set to tsting routes

--- event-plus-frontend/src/App.js
+++ event-plus-frontend/src/App.js
@@ -12,7 +12,30 @@ import LiveEvent from "./pages/LiveEvent";
 
 
 
+// testing routes
+
+export default function App() {
+  return (
+    <>
+    <Header />
+    <Routes>
+      <Route path="/" element={<LiveEvent />} />
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/create-event" element={<CreateEventForm />} />
+      <Route path="/event/:id" element={<Event />} />
+      <Route path="/update-event/:id" element={<UpdateEventForm />} />
+      <Route path="*" element={<Notfound />} />
+    </Routes>
+    <Footer />
+    </>
+  );
+}
+
+
+
 // OG routes (configure below)
+/*
 export default function App() {
   return (
     <>
@@ -31,26 +54,4 @@ export default function App() {
     </>
   );
 }
-
-
-
-// testing routes
-/*
-export default function App() {
-  return (
-    <>
-    <Header />
-    <Routes>
-      <Route path="/" element={<LiveEvent />} />
-      <Route path="/signin" element={<SignIn />} />
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/create-event" element={<CreateEventForm />} />
-      <Route path="/event/:id" element={<Event />} />
-      <Route path="/update-event/:id" element={<UpdateEventForm />} />
-      <Route path="*" element={<Notfound />} />
-    </Routes>
-    <Footer />
-    </>
-  );
-}
-  */
+*/
