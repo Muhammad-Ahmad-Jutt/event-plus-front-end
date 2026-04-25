1b1c1b7d930949213d68a0c4e844b98c8b0cd832
gouravray <gourav.sankar93@gmail.com>
2026-04-25 09:44:27 +0100

    [BUG FIX] Protected route redirection was on for logged in users as well. Patched this to wait for auth token loading to allow aauthenticated users from accessing the routes.

--- event-plus-frontend/src/components/ProtectedRoute.js
+++ event-plus-frontend/src/components/ProtectedRoute.js
@@ -1,17 +1,17 @@
-/*
-REFERENCE (Following link is referred for conceptual reference): 
-- Gathoni, M. (2023) Protected routes in React. Available at: https://www.makeuseof.com/create-protected-route-in-react/?utm_source=chatgpt.com
-*/
-
 import { useContext } from "react";
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
 
 export default function ProtectedRoute({ children }) {
-  const { token } = useContext(AuthContext);
+  const { token, authLoading } = useContext(AuthContext);
+  const location = useLocation();
+
+  if (authLoading) {
+    return null;
+  }
 
   if (!token) {
-    return <Navigate to="/" replace />;
+    return <Navigate to="/signin" replace state={{ from: location }} />;
   }
 
   return children;
